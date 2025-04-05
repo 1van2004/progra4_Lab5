76b61d85cc1ac3e068806338ae2e51eb5b5cbf10
carito0704 <maria.gonzalez.jimenez@est.una.ac.cr>
2025-04-05 10:37:45 -0600

    orden checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -20,30 +20,35 @@ const newTodo = {
   description: todoDescription
 }
   
-tempTodoList.push(newTodo)
+tempTodoList.unshift(newTodo)
 
 setTodoList(tempTodoList)
   }
 
   return (
-    <>
+    
     <div style={{border: '1px solid red', padding: 10}}>
 
-<div>
-  <input 
-  value={todoDescription}
-onChange={handleChange}
-  style= {{marginRight: 10}}/>
-  <button onClick={handleCLick}>Add Item</button>
-</div>
+     <div>
+       <input 
+         type='text'
+         value={todoDescription}
+         onChange={handleChange}
+         style= {{marginRight: 10}}/>
+        <button onClick={handleCLick}>Add Item</button>
+     </div>
 
+     <div> TODOs Here:</div>
+     <ul>
+       {todoList.map((todo, index) => {
+         return <li key={index}>
+                 <input type='checkbox' />
+                 {todo.description}
+             </li>
+        })}
+     </ul>
 
-<div> TODOs Here:</div>
-<div>{ JSON.stringify(todoList) }</div>
-
-
-    </div>
-    </>
+   </div>
   )
 }
 
