935f16e9713bc61841c569d899e58bc16da660ca
carito0704 <maria.gonzalez.jimenez@est.una.ac.cr>
2025-04-05 20:13:09 -0600

    crud y local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,67 @@
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
-interface Todo{
+interface Todo {
   description: string
 }
 
+const loadFromLocalStorage = (): Todo[] => {
+  const data = localStorage.getItem('todos')
+  return data ? JSON.parse(data) : []
+}
+
+const saveToLocalStorage = (todos: Todo[]) => {
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
 function App() {
+  const [todoDescription, setTodoDescription] = useState('')
+  const [todoList, setTodoList] = useState<Todo[]>([])
 
-const [todoDescription, setTodoDescription] = useState('')
-const [todoList, setTodoList] = useState<Todo[]>([])
+  // Cargar datos al iniciar
+  useEffect(() => {
+    const storedTodos = loadFromLocalStorage()
+    setTodoList(storedTodos)
+  }, [])
 
-const handleChange = (e: any) => {
-  setTodoDescription(e.target.value)
-}
+  const handleChange = (e: any) => {
+    setTodoDescription(e.target.value)
+  }
 
-  const handleCLick = () => {
-const tempTodoList = [...todoList]
-const newTodo = {
-  description: todoDescription
-}
-  
-tempTodoList.unshift(newTodo)
+  const handleClick = () => {
+    if (todoDescription.trim() === '') return
 
-setTodoList(tempTodoList)
+    const newTodo: Todo = { description: todoDescription }
+    const updatedList = [newTodo, ...todoList]
+    setTodoList(updatedList)
+    saveToLocalStorage(updatedList)
+    setTodoDescription('')
   }
 
   return (
-    
-    <div style={{border: '1px solid red', padding: 10}}>
+    <div style={{ border: '1px solid red', padding: 10 }}>
+      <div>
+        <input
+          type='text'
+          value={todoDescription}
+          onChange={handleChange}
+          style={{ marginRight: 10 }}
+        />
+        <button onClick={handleClick}>Add Item</button>
+      </div>
 
-     <div>
-       <input 
-         type='text'
-         value={todoDescription}
-         onChange={handleChange}
-         style= {{marginRight: 10}}/>
-        <button onClick={handleCLick}>Add Item</button>
-     </div>
-
-     <div> TODOs Here:</div>
-     <ul>
-       {todoList.map((todo, index) => {
-         return <li key={index}>
-                 <input type='checkbox' />
-                 {todo.description}
-             </li>
-        })}
-     </ul>
-
-   </div>
+      <div>TODOs Here:</div>
+      <ul>
+        {todoList.map((todo, index) => (
+          <li key={index}>
+            <input type='checkbox' />
+            {todo.description}
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
 export default App
+
